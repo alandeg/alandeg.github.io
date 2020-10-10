1ddcd3ff9dd65a0fe1f894cae5dbf0cad9c18ba7
Alan DeGabriele <alan.deg@gmail.com>
2020-10-10 23:26:19 +0200

    release: 0.0.0
    # Conflicts:
    #	index.html

---
+++ precache-manifest.9d1a05cee9c3a02451f2734df378a0aa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e96a8af8f143047f46f",
+    "url": "/css/app.d32ebfb6.css"
+  },
+  {
+    "revision": "d11030c0273b19515232",
+    "url": "/css/chunk-vendors.d3d0b2c5.css"
+  },
+  {
+    "revision": "d66cf562953c78fcce058d87f761a672",
+    "url": "/img/dummy-logo.png"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/vue-logo.png"
+  },
+  {
+    "revision": "dfb024b5ee9742fa9a05ce4e0bd26922",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e96a8af8f143047f46f",
+    "url": "/js/app.52e496c0.js"
+  },
+  {
+    "revision": "d11030c0273b19515232",
+    "url": "/js/chunk-vendors.91709443.js"
+  },
+  {
+    "revision": "f38e05fd80c23b1aa17c3ac5c993d48d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
